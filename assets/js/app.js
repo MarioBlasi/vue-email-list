e0e944d09e7abed5b4c9a73bafb8f51a0304a5d2
MarioBlasi <mblasi509@gmail.com>
2023-04-11 13:43:45 +0100

    {{ index + 1 }} - {{ email }}

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,24 @@ createApp({
   data() {
     return {
       message: "Email list",
-      email: null,
+      emails: [],
     };
   },
-  mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((response) => {
-        console.log(response);
-        this.email = response.data.response;
-      });
+  created() {
+    for (let i = 0; i < 10; i++) {
+      this.getEmail();
+    }
+  },
+  methods: {
+    //async per dichiarare che è una funzione asincrona.
+    async getEmail() {
+      // Il metodo fetch prende l'URL dell'API come argomento e restituisce una
+      //promessa contenente la risposta alla richiesta.
+      const response = await fetch(
+        "https://flynn.boolean.careers/exercises/api/random/mail"
+      );
+      const data = await response.json();
+      this.emails.push(data.response);
+    },
   },
 }).mount("#app");
